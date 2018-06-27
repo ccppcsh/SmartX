c08e0bdbd41c9c4a701878614eac310248bb80de
Unknown <viacheslav.lylin@gmail.com>
2018-06-27 09:06:48 +0300

    Added new test to check if broken data will emit the error event

--- UnitTests/TestTempHumSHT20ParcelHandler/TestTempHumSHT20ParcelHandler.cpp
+++ UnitTests/TestTempHumSHT20ParcelHandler/TestTempHumSHT20ParcelHandler.cpp
@@ -13,7 +13,9 @@ private Q_SLOTS:
     void init();
     void cleanup();
     void handleParcel_ValidParcel_SignalsEmited();
+    void handleParcel_ValidParcel_SensorValuesRight_data();
     void handleParcel_ValidParcel_SensorValuesRight();
+    void handleParcel_InvalidData_ErrorSignalEmited_data();
     void handleParcel_InvalidData_ErrorSignalEmited();
 signals:
     void mockParserSignalParcelBuilt(const Parcel& parcel);
@@ -55,16 +57,76 @@ void TestTempHumSHT20ParcelHandler::handleParcel_ValidParcel_SignalsEmited()
 
 }
 
+void TestTempHumSHT20ParcelHandler::handleParcel_ValidParcel_SensorValuesRight_data()
+{
+    // Setup table
+    QTest::addColumn<QString>("parcelBytes");
+    QTest::addColumn<float>("tempValueExpected");
+    QTest::addColumn<float>("humidityValueExpected");
+
+    // Add cases
+    QTest::newRow("Parcel with positive temperature and humidity") << "Temp=26.170 Humi=51.240" << (float) 26.170 << (float) 51.240;
+    QTest::newRow("Parcel without point after value") << "Temp=26 Humi=51" << (float) 26.0 << (float) 51.0;
+    QTest::newRow("Parcel with negative temperature") << "Temp=-40.124 Humi=51" << (float) -40.124 << (float) 51.0;
+}
+
 void TestTempHumSHT20ParcelHandler::handleParcel_ValidParcel_SensorValuesRight()
 {
-    // Handle parcel like "TEMP=29.4 HUM=75"
-    // Temp = 29
-    // Temp = 29,0
-    // Temp = -40
+    // Arrange
+    QFETCH(QString, parcelBytes);
+    QFETCH(float, tempValueExpected);
+    QFETCH(float, humidityValueExpected);
+
+    Parcel parcel;
+    for (int i = 0; i < parcelBytes.length(); i++)
+        parcel << parcelBytes.at(i).toLatin1();
+
+    QSignalSpy spy(handler, SIGNAL(eventOccured(int)));
+
+    // Act
+    emit mockParserSignalParcelBuilt(parcel);
+
+    // Assert
+    QCOMPARE(spy.count(), 2);
+
+    QList<QVariant> arguments = spy.takeFirst();
+
+    QCOMPARE((int) TempHumSHT20ParcelHandler::TEMPERATURE_VALUE_UPDATED, arguments.at(0).toInt());
+    QCOMPARE(tempValueExpected, handler->getTempValue());
+
+    arguments = spy.takeFirst();
+    QCOMPARE((int) TempHumSHT20ParcelHandler::HUMIDITY_VALUE_UPDATED, arguments.at(0).toInt());
+    QCOMPARE(humidityValueExpected, handler->getHumidityValue());
+}
+
+void TestTempHumSHT20ParcelHandler::handleParcel_InvalidData_ErrorSignalEmited_data()
+{
+    QTest::addColumn<QString>("parcelBytes");
+
+    //Add cases
+    QTest::newRow("Error in temperature value") << "Temp=26e170 Humi=51.240";
+    QTest::newRow("Any char instead of first equal char") << "Temp!26.170 Humi=51.240";
+    QTest::newRow("Any char instead of second equal char") << "Temp=26.170 Humi!51.240";
 }
 void TestTempHumSHT20ParcelHandler::handleParcel_InvalidData_ErrorSignalEmited()
 {
-    // Temp = 25.c Hum=dfdsf
+    // Arrange
+    QFETCH(QString, parcelBytes);
+    Parcel parcel;
+    for (int i = 0; i < parcelBytes.length(); i++)
+        parcel << parcelBytes.at(i).toLatin1();
+
+    QSignalSpy spy(handler, SIGNAL(eventOccured(int)));
+
+    // Act
+    emit mockParserSignalParcelBuilt(parcel);
+
+    // Assert
+    QCOMPARE(spy.count(), 1);
+
+    QList<QVariant> arguments = spy.takeFirst();
+
+    QCOMPARE((int) TempHumSHT20ParcelHandler::PARCEL_NOT_RECOGNIZED, arguments.at(0).toInt());
 }
 
 QTEST_APPLESS_MAIN(TestTempHumSHT20ParcelHandler)
