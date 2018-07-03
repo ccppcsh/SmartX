a65d371ce8bbaa17d6b1d08547397dd9b23941d4
Unknown <viacheslav.lylin@gmail.com>
2018-07-03 14:56:37 +0300

    Added implementation of all methods

--- Sensors/BaseSensor.cpp
+++ Sensors/BaseSensor.cpp
@@ -1,15 +1,45 @@
+/**
+ * @file    BaseSensor.h
+ * @date    2018-07-03
+ * @author  sauber
+ * @brief   The BaseSensor implementation class file
+ */
+
 #include "BaseSensor.h"
 
-BaseSensor::BaseSensor(QObject *parent) : QObject(parent)
+BaseSensor::BaseSensor(QObject *parent) : BaseDevice(NULL, parent)
 {
-    mSensorsDataList.push_back(* (new SensorData(QString("Temperature"))));
-    mSensorsDataList[0].setValue(35.4);
+
 }
 
-BaseSensor::BaseSensor(QString desc, QObject *parent): QObject(parent)
+BaseSensor::BaseSensor(BaseCommunicationInterface *comInterface, BaseParser *parser, BaseParcelHandler *handler, QString desc, QObject *parent) : BaseDevice(comInterface, parent)
 {
+
+    mParcelHandler = handler;
+    mParser = parser;
     mSensorDesc = desc;
+    if ((mParser == NULL) || (mParcelHandler == NULL) || (mComInterface == NULL))
+    {
+        // TODO: Error handling
+    }
+
+    // Connect signals and slots
+    // Interface --> Parser --> ParcelHandler --> Sensor
+    QObject::connect(mComInterface, SIGNAL(onDataReceived(QByteArray)), mParser, SLOT(onDataReceived(QByteArray)));
+    QObject::connect(mParser, SIGNAL(onParcelBuilt(Parcel)), mParcelHandler, SLOT(onParcelBuilt(Parcel)));
+    QObject::connect(mParcelHandler, SIGNAL(eventOccured(int)), this, SLOT(onParcelHandlerEvent));
 }
+
+BaseSensor::~BaseSensor()
+{
+    if (mComInterface != NULL)
+        mComInterface->disconnect();
+
+    QObject::disconnect(mComInterface, SIGNAL(onDataReceived(QByteArray)), mParser, SLOT(onDataReceived(QByteArray)));
+    QObject::disconnect(mParser, SIGNAL(onParcelBuilt(Parcel)), mParcelHandler, SLOT(onParcelBuilt(Parcel)));
+    QObject::disconnect(mParcelHandler, SIGNAL(eventOccured(int)), this, SLOT(onParcelHandlerEvent));
+}
+
 
 const QList<SensorData> &BaseSensor::getSensorsDataList()
 {
@@ -26,8 +56,32 @@ void BaseSensor::setSensorDesc(QString desc)
     mSensorDesc = desc;
 }
 
-QString BaseSensor::getSensorDesc()
+QString BaseSensor::getSensorDesc() const
 {
     return mSensorDesc;
+}
+
+void BaseSensor::connect()
+{
+    auto ec = mComInterface->connect();
+
+    if (ec == BaseCommunicationInterface::ErrorCode::OK)
+        mIsDeviceConnected = true;
+    else
+        mIsDeviceConnected = false;
+
+    return;
+}
+
+void BaseSensor::disconnect()
+{
+    mComInterface->disconnect();
+
+    mIsDeviceConnected = false;
+}
+
+bool BaseSensor::isConnected()
+{
+    return mIsDeviceConnected;
 }
 
--- Sensors/BaseSensor.h
+++ Sensors/BaseSensor.h
@@ -1,10 +1,20 @@
 #ifndef BASESENSOR_H
 #define BASESENSOR_H
 
+/**
+ * @file    BaseSensor.h
+ * @date    2018-07-03
+ * @author  sauber
+ * @brief   The BaseSensor class header file
+ */
+
 #include <QObject>
 #include "SensorData.h"
+#include "Device/BaseDevice.h"
+#include "ProtocolParser/BaseParcelHandler.h"
+#include "ProtocolParser/BaseParser.h"
 
-class BaseSensor : public QObject
+class BaseSensor : public BaseDevice
 {
     Q_OBJECT
 public:
@@ -15,19 +25,58 @@ public:
         SENSOR_OK
     };
     explicit BaseSensor(QObject *parent);
-    BaseSensor(QString desc, QObject *parent);
+    BaseSensor(BaseCommunicationInterface* comInterface, BaseParser* parser, BaseParcelHandler* handler, QString desc = "", QObject *parent = NULL);
+    ~BaseSensor();
+    /**
+     * @brief getSensorsDataList - returns a list with all values of sensor
+     * @return - list with all sensor values
+     */
     const QList<SensorData>& getSensorsDataList();
+    /**
+     * @brief getSensorStatus - return current status of sensor
+     * @return - sensor status
+     */
     BaseSensor::SensorStatus getSensorStatus();
+    /**
+     * @brief setSensorDesc - sets a description of sensor
+     * @param desc - string which contains sensor description
+     */
     void setSensorDesc(QString desc);
-    QString getSensorDesc();
+    /**
+     * @brief getSensorDesc - returns a description of sensor
+     * @return - string with description
+     */
+    QString getSensorDesc() const;
 private:
+    QString mSensorDesc = "";
+    BaseParcelHandler* mParcelHandler = NULL;
+    BaseParser* mParser = NULL;
+protected:
     QList<SensorData> mSensorsDataList = QList<SensorData>();
     BaseSensor::SensorStatus mStatus = SensorStatus::SENSORE_INACTIVE;
-    QString mSensorDesc = "";
+
+    /**
+     * @brief connect - tries to connect with device over communication interface
+     */
+    virtual void connect();
+    /**
+     * @brief disconnect - disconnects from device and disposes all used resources
+     */
+    virtual void disconnect();
+    /**
+     * @brief isConnected - returns status of device
+     * @return - true, if connected; otherwise - false
+     */
+    virtual bool isConnected();
 
 signals:
 
 public slots:
+    /**
+     * @brief onParcelHandlerEvent - sensor specific handler by handling a parcel
+     * @param event - type of event which is defined for each implemented sensor
+     */
+    virtual void onParcelHandlerEvent(int event) = 0;
 };
 
 
