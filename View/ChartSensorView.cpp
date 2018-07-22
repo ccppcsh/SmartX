80a3e759da912bf041a012ebb0368fc754d2f621
Unknown <viacheslav.lylin@gmail.com>
2018-07-22 17:34:48 +0200

    Stub implementation to show sensor values in runtime

---
+++ View/ChartSensorView.cpp
@@ -0,0 +1,6 @@
+#include "ChartSensorView.h"
+
+ChartSensorView::ChartSensorView()
+{
+
+}
---
+++ View/ChartSensorView.h
@@ -0,0 +1,11 @@
+#ifndef CHARTSENSORVIEW_H
+#define CHARTSENSORVIEW_H
+
+
+class ChartSensorView
+{
+public:
+    ChartSensorView();
+};
+
+#endif // CHARTSENSORVIEW_H
