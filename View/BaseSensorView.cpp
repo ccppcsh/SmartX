16fe7eba9464fcef5eea5499f7710b1a9bdbd7da
Unknown <viacheslav.lylin@gmail.com>
2018-07-22 17:37:59 +0200

    First realization of sensor view without data update

--- View/BaseSensorView.cpp
+++ View/BaseSensorView.cpp
@@ -1,6 +1,49 @@
 #include "BaseSensorView.h"
 
-BaseSensorView::BaseSensorView()
-{
 
+BaseSensorView::BaseSensorView(BaseSensor *sensor)
+{
+    mSensor = sensor;
+
+    // Setup layout
+    this->setSpacing(0);
+    this->setMargin(0);
+    this->setAlignment(Qt::AlignTop);
+
+
+    // Add label description
+    QLabel* pLabelDesc = new QLabel();
+    pLabelDesc->setText(mSensor->getSensorDesc()); 
+    this->addWidget(pLabelDesc, 0, Qt::AlignTop);
+
+
+    /*QLabel* pLabelDescTest = new QLabel();
+    pLabelDescTest->setText(mSensor->getSensorDesc());
+    this->addWidget(pLabelDescTest, 0, Qt::AlignTop);*/
+
+    // Add spacer
+    QFrame* line = new QFrame();
+    line->setObjectName(QString::fromUtf8("line"));
+    line->setGeometry(QRect(320, 150, 118, 3));
+    line->setFrameShape(QFrame::HLine);
+    line->setFrameShadow(QFrame::Sunken);
+    this->addWidget(line, 0, Qt::AlignTop);
+
+    auto sensorList = mSensor->getSensorsDataList();
+    for (int i = 0; i < sensorList.length(); i++)
+    {
+        addSensorValueChart(sensorList[i].getName());
+    }
+}
+
+void BaseSensorView::addSensorValueChart(QString desc)
+{
+    ChartSensorView* chart = new ChartSensorView();
+    chart->setTitle(desc);
+    chart->setAnimationOptions(QChart::AllAnimations);
+
+     QChartView* chartView = new QChartView(chart);
+     chartView->setRenderHint(QPainter::Antialiasing);
+
+     this->addWidget(chartView);
 }
--- View/BaseSensorView.h
+++ View/BaseSensorView.h
@@ -1,11 +1,33 @@
 #ifndef BASESENSORVIEW_H
 #define BASESENSORVIEW_H
+/**
+ * @file    BaseSensor.h
+ * @date    2018-07-020
+ * @author  sauber
+ * @brief   The BaseSensorView class header file
+ * This class displays the current values of sensors. The sensors data will be displayed on QChart like a time-graphic wit hcurrent value.
+ */
 
+//QT Includes
+#include <QVBoxLayout>
+#include <QtCharts/QChartView>
+#include <QLabel>
 
-class BaseSensorView
+//SmartX includes
+#include "Sensors/BaseSensor.h"
+#include "ChartSensorView.h"
+
+class BaseSensorView : public QVBoxLayout
 {
 public:
-    BaseSensorView();
+    BaseSensorView(BaseSensor* sensor);
+private:
+    BaseSensor* mSensor;
+    /**
+     * @brief addSensorValueChart - generates sensor chart view for sensor parameter
+     * @param desc - name of measured parameter by sensor
+     */
+    void addSensorValueChart(QString desc);
 };
 
 #endif // BASESENSORVIEW_H
