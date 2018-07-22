6b3cb1797593e256bdac0a445a2ea64989322d64
Unknown <viacheslav.lylin@gmail.com>
2018-07-22 17:37:24 +0200

    Stub implementation added

--- View/ChartSensorView.cpp
+++ View/ChartSensorView.cpp
@@ -1,6 +1,29 @@
 #include "ChartSensorView.h"
+#include <QtCharts/QAbstractAxis>
+#include <QtCharts/QSplineSeries>
+#include <QtCharts/QValueAxis>
 
-ChartSensorView::ChartSensorView()
+ChartSensorView::ChartSensorView(QGraphicsItem *parent, Qt::WindowFlags wFlags):
+    QChart(QChart::ChartTypeCartesian, parent, wFlags)
 {
+    mSerie = new QSplineSeries(this);
+    QPen pen(Qt::red);
+    pen.setWidth(3);
 
+    mSerie->setPen(pen);
+
+    addSeries(mSerie);
+    createDefaultAxes();
+    this->legend()->hide();
+
+    mAxis = new QValueAxis();
+    setAxisX(mAxis, mSerie);
+
+    axisX()->setRange(0, 10);
+    axisY()->setRange(0, 10);
+    mSerie->append(5, 6);
+    mSerie->append(6, 7);
+    mSerie->append(7, 8);
+    mSerie->append(8, 9);
+    mSerie->append(9, 10);
 }
--- View/ChartSensorView.h
+++ View/ChartSensorView.h
@@ -1,11 +1,36 @@
 #ifndef CHARTSENSORVIEW_H
 #define CHARTSENSORVIEW_H
 
+/**
+ * @file    ChartSensorView.h
+ * @date    2018-07-22
+ * @author  sauber
+ * @brief   The ChartSensorView class header file
+ * Creates a chart for a sensor.
+ */
 
-class ChartSensorView
+#include <QtCharts/QChart>
+
+
+QT_CHARTS_BEGIN_NAMESPACE
+class QSplineSeries;
+class QValueAxis;
+QT_CHARTS_END_NAMESPACE
+
+QT_CHARTS_USE_NAMESPACE
+
+class ChartSensorView : public QChart
 {
+    Q_OBJECT
 public:
-    ChartSensorView();
+    ChartSensorView(QGraphicsItem *parent = 0, Qt::WindowFlags wFlags = 0);
+public slots:
+    //void valueUpdated();
+private:
+    QSplineSeries* mSerie;
+    QStringList mTitles;
+    QValueAxis* mAxis;
+
 };
 
 #endif // CHARTSENSORVIEW_H
